937aa091c7184304b6361e2f1f9763d205ce9404
Hoang Tien Minh <htminh.1001@gmail.com>
2026-05-01 22:05:25 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Print("$ ")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		command := scanner.Text()
+		fmt.Printf("%s: command not found", command)
+	}
 }
