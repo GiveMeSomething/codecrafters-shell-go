66163d8f3c5c585119e2e5147ded889c4c80af16
Hoang Tien Minh <htminh.1001@gmail.com>
2026-05-01 22:20:49 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,7 +12,12 @@ type ShellCommand string
 const (
 	CommandExit ShellCommand = "exit"
 	CommandEcho ShellCommand = "echo"
+	CommandType ShellCommand = "type"
 )
+
+func (cmd ShellCommand) IsBuiltIn() bool {
+	return cmd == CommandExit || cmd == CommandEcho || cmd == CommandType
+}
 
 func handleCommand(command ShellCommand, args []string) {
 	switch command {
@@ -21,6 +26,14 @@ func handleCommand(command ShellCommand, args []string) {
 		return
 	case CommandEcho:
 		fmt.Println(strings.Join(args, " "))
+		return
+	case CommandType:
+		typeArg := ShellCommand(args[0])
+		if typeArg.IsBuiltIn() {
+			fmt.Printf("%s is a shell builtin\n", typeArg)
+			return
+		}
+		fmt.Printf("%s: not found\n", typeArg)
 		return
 	}
 
