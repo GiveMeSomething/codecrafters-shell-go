dac70070bd38c52183def7211399c7588655c2a6
Hoang Tien Minh <htminh.1001@gmail.com>
2026-05-02 09:03:56 +0700

    refactor: extract search for executable logic

--- app/command/cmd_type.go
+++ app/command/cmd_type.go
@@ -2,9 +2,6 @@ package command
 
 import (
 	"fmt"
-	"os"
-	"path"
-	"strings"
 )
 
 func HandleTypeCommand(args []string) {
@@ -16,24 +13,11 @@ func HandleTypeCommand(args []string) {
 		return
 	}
 
-	pathEnv := os.Getenv("PATH")
-	folders := strings.SplitSeq(pathEnv, ":")
-	for folderName := range folders {
-		checkPath := path.Join(folderName, targetCmd)
-		fileInfo, err := os.Stat(checkPath)
-		if err != nil {
-			// Ignore error, continue checking
-			continue
-		}
-
-		if !IsExecAny(fileInfo.Mode()) {
-			// If the file exists but lacks execute permissions, skip it and continue to the next directory
-			continue
-		}
-
-		fmt.Printf("%s is %s\n", targetCmd, checkPath)
+	foundPath := SearchExecutable(targetCmd)
+	if foundPath == nil {
+		fmt.Printf("%s: not found\n", targetCmd)
 		return
 	}
 
-	fmt.Printf("%s: not found\n", targetCmd)
+	fmt.Printf("%s is %s\n", targetCmd, *foundPath)
 }
--- app/command/utils.go
+++ app/command/utils.go
@@ -1,6 +1,10 @@
 package command
 
-import "os"
+import (
+	"os"
+	"path"
+	"strings"
+)
 
 // UNIX permission bits
 // Owner - Group - Other
@@ -20,4 +24,25 @@ func IsExecOther(mode os.FileMode) bool {
 
 func IsExecAny(mode os.FileMode) bool {
 	return mode&0111 != 0
+}
+
+func SearchExecutable(targetCmd string) *string {
+	pathEnv := os.Getenv("PATH")
+	folders := strings.SplitSeq(pathEnv, ":")
+	for folderName := range folders {
+		checkPath := path.Join(folderName, targetCmd)
+		fileInfo, err := os.Stat(checkPath)
+		if err != nil {
+			// Ignore error, continue checking
+			continue
+		}
+
+		if !IsExecAny(fileInfo.Mode()) {
+			// If the file exists but lacks execute permissions, skip it and continue to the next directory
+			continue
+		}
+
+		return &checkPath
+	}
+	return nil
 }
